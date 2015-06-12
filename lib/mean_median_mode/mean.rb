09463ef95cfa55346d35dd4964a594dc735c8fd8
Dhara Joshi <djjoshidhara@gmail.com>
2015-06-12 14:27:46 +0530

    working on Mode

--- lib/mean_median_mode/mean.rb
+++ lib/mean_median_mode/mean.rb
@@ -1,7 +1,20 @@
 module MeanMedianMode
   class Mean < Array
     def initialize(*number_array)
+      puts '*'*80
       puts "Mean called"
+      puts '*'*80
+      num_arr =  *number_array
+      total = num_arr.count
+      if total
+        sorted_array = num_arr.sort
+        puts '*'*80
+        puts sorted_array
+        puts '*'*80
+      end
+      puts '*'*80
+      puts 'after if'
+      puts '*'*80
     end
   end
 end
