aff59ad51f293d9da8910039edae0b853fe1faa2
Dhara Joshi <djjoshidhara@gmail.com>
2015-06-15 13:23:37 +0530

    gem created :: final changes for mode

--- lib/mean_median_mode.rb
+++ lib/mean_median_mode.rb
@@ -1,14 +1,16 @@
 require "mean_median_mode/mean"
 require "mean_median_mode/median"
+require "mean_median_mode/mode"
 require "mean_median_mode/version"
 
 module MeanMedianMode
   class Base
-    attr_reader :mean_result, :median_result
+    attr_reader :mean_result, :median_result, :mode_result
 
     def initialize(args)
       @mean_result = MeanMedianMode::Mean.new(args)
       @median_result = MeanMedianMode::Median.new(args)
+      @mode_result = MeanMedianMode::Mode.new(args)
     end
   end
 end
